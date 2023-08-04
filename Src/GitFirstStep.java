e30ede019ab24e49d94732ffc87e1ca7cc448d51
JzomitiZumatti <evgeniyshinkar@gmail.com>
2023-08-05 00:16:29 +0300

    Add second output.

--- Src/GitFirstStep.java
+++ Src/GitFirstStep.java
@@ -1,5 +1,6 @@
 public class GitFirstStep {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
+        System.out.println("Hello Git community, I'm here!");
     }
 }
